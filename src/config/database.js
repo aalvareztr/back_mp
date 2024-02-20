625c678fb180b52d96662529a7b79479aae976eb
aalvareztr <aalvarez@wecom.global>
2024-02-20 11:08:26 -0300

    first commit

--- src/config/database.js
+++ src/config/database.js
@@ -2,6 +2,6 @@ import mysql from 'mysql2/promise';
 import 'dotenv/config'
 
 
-export const connection = await mysql.createConnection('mysql://rvflhhcjmyhu47ei0uj9:pscale_pw_TrtSV4YivNxreU9dBtSPBgupep5z2B21QjUKuPVLU5A@aws.connect.psdb.cloud/miasesordb?ssl={"rejectUnauthorized":true}')
+export const connection = await mysql.createConnection('mysql://9zarfabppw4guoget5a4:pscale_pw_tYkGBkQ2V4HGO9m0XRsr1dmPJmQ7zT3I1Rf7xOd6ICs@aws.connect.psdb.cloud/miasesordb?ssl={"rejectUnauthorized":true}')
 
 
