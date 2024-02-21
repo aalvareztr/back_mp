1f49c85cf48039ad94ca3be3b8c2a7e4afb652fd
aalvareztr <156945806+aalvareztr@users.noreply.github.com>
2024-02-21 09:32:30 -0300

    Update routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -16,38 +16,6 @@ route.get('/',async(req,res)=>{
 })
 
 ///////////////////////// LOGIN ////////////////////////////////////
-/*
-route.post('/api/form',async(req,res)=>{
-    const { rut,password } = req.body;
-    try{
-        const __dirname = path.dirname(new URL(import.meta.url).pathname);
-        const filePath = path.resolve(__dirname, 'data.json');
-        const data = await fs.readFile(filePath, 'utf8');
-        const jsonData = JSON.parse(data);
-
-        const findClient = jsonData.find((item)=>item.rut === rut && item.clave_si === password)
-        if(findClient){
-            const [pagos] = await connection.execute('SELECT * FROM pagos_marcados WHERE idCliente = ?',[rut]);
-            return res
-            .status(200)
-            .json({ok:true,data:findClient,pagos})
-        }else{
-            return res
-            .status(400)
-            .json({ok:false,code:1,message:'ruto o contrasena incorrectos'})
-        }
-
-    }catch(err){
-        console.log(err)
-        return res
-        .status(400)
-        .json({ok:false,code:0,err})
-    }
-
-})
-*/
-
-///////////////////////// LOGIN ////////////////////////////////////
 
 route.post('/api/form',async(req,res)=>{
     const { rut,password } = req.body;
